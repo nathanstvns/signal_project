8ed318ef37ca115ab482c2ca03569a882d8b88ca
I6398886 <nathan.stevens@student.maastrichtuniversity.nl>
2025-04-20 21:25:35 +0200

    main class

---
+++ src/main/java/com/cardio_generator/Main.java
@@ -0,0 +1,13 @@
+package com.cardio_generator;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 0 && args[0].equals("DataStorage")) {
+            com.data_management.DataStorage.main(new String[]{});
+        } else {
+            HealthDataSimulator.main(new String[]{});
+        }
+    }
+}
